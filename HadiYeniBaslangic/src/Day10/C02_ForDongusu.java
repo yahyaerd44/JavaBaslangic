57050f07906bbe99912c0638415d87e1ac0e3016
HadiARICI <132011080+HadiARICI@users.noreply.github.com>
2023-05-01 00:07:02 +0300

    30.04
    2023

---
+++ HadiYeniBaslangic/src/Day10/C02_ForDongusu.java
@@ -0,0 +1,18 @@
+package Day10;
+
+public class C02_ForDongusu {
+    public static void main(String[] args) {
+
+        /*
+
+        for (int i = 0; i < Koşul ; i++) {
+        ---------       ----------  ------
+           1                2         3
+           for döngüsünde 1. bir kere çalışır
+           2.true ise parantez içi çalışır sonra 3. kısımda artırma yada azaltma olur
+           gene 2.kısım koşul true ise gene aynı sıralama devam eder sonuc flase olana kadar
+                    }
+         */
+
+    }
+}
